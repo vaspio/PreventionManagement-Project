47345e36b64e3b011afc63ee29534b73aa49bd3e
Dimitris <dimitriskl@debian-BULLSEYE-live-builder-AMD64>
2021-12-14 02:58:11 +0200

    organize/cleanup code, use mqtt message info for event insertion and adjusted for new table

--- EdgeServer.java
+++ EdgeServer.java
@@ -11,59 +11,52 @@ import java.util.Scanner;
 import java.util.Date;
 import java.util.Properties;
 
-
 import org.eclipse.paho.client.mqttv3.*;
 
+
 public class EdgeServer {
+    
+    // Tracking Mqtt messaging statistics
+    static int messages_received = 0;
     static int messages_completed = 0;
+    static double android_position_x = 35.58;
+    static double android_position_y = 28.12;
+    
+    // Limits and thresholds
+    static double smoke_min_value = 0;
+    static double smoke_max_value = 0.25;
+    static double smoke_upper_threshold = 0.14;
+    static double gas_min_value = 0;
+    static double gas_max_value = 11;
+    static double gas_upper_threshold = 1.0065; // 9.15%
+    static double temperature_min_value = -5;
+    static double temperature_max_value = 80;
+    static double temperature_upper_threshold = 50;
+    static double radiation_min_value = 0;
+    static double radiation_max_value = 11;
+    static double radiation_upper_threshold = 6;
 
 	// Init server
 	public static void main(String[] args) {
 		connectToServer();
 	}
 
-    public static Connection getConn(){
-        Connection conn = null;
-        try{
-
-            /* db parameters */
-            String driver = "com.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://localhost:3306/preventiondb?characterEncoding=latin1";
-            String user = "root";
-            String password = "Project123!";
-
-            /* connection */
-            conn = DriverManager.getConnection(url, user, password);
-            
-        } 
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-
-        return conn;
-    }
-
 
     public static void connectToServer() {
 
-		System.out.println("Setting the Server info...");
-		System.out.println("Setting Broker IP...");
+        // Server settings
 		String broker = "tcp://localhost:1883";
-		System.out.println("Setting client ID for Mqtt client...");
 		String clientId = "JavaServer";
 		MqttClientPersistence persistence = null;
-
-		String serverUrl;
 		MqttAsyncClient sampleClient;
 
-		// topics
-		System.out.println("Setting the topics...");
+		// Topics
 		String publishTopicAndroid = "pub_android";
 		String publishTopicIot = "pub_iot";
 		String subscribeTopicAndroid = "sub_android";
 		String subscribeTopicIot = "sub_iot";
-        System.out.println("Setting topics for the android and iot to subscribe and publish to...");
 
+        // Establish mqtt client
 		try {
 			sampleClient = new MqttAsyncClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
@@ -94,64 +87,39 @@ public class EdgeServer {
             System.out.println("msg "+me.getMessage());
             System.out.println("loc "+me.getLocalizedMessage());
             return ;
-        }
-
-
-        /*************
-        ** DATABASE **
-        *************/
-        
-        Connection conn = getConn();
-        System.out.println("Connection to Database has been successful!");
-
-        // finally{
-        //     try {
-        //         if(conn != null){
-        //             conn.close();
-        //         }
-        //     } 
-        //     catch(SQLException ex){
-        //         System.out.println(ex.getMessage());
-        //     }
-        // }
-
+        }        
 	}
 
 
+    // Callback functionality to handle incoming mqtt messages
     static MqttCallback mqttCallback = new MqttCallback() {
         @Override
         public void connectionLost(Throwable connection) {
             System.out.println("\n\nConnection lost..");
-            return;
         }
 
         @Override
         public void messageArrived(String topic, MqttMessage mqttMessage) throws MqttException {
-            String message = new String(mqttMessage.getPayload());
-            System.out.println("Topic:" + topic + " Message:" + message + " qos " + mqttMessage.getQos());
+            // Get message payload
+            String messageFromPublish = new String(mqttMessage.getPayload());
+            System.out.println("Topic:" + topic + " Message:" + messageFromPublish);
+            messages_received++;
 
-            String messageFromPublish = "30.01|29.99|68.86|0.15|1.2|55|7";
- 
-            String[] words = messageFromPublish.split("\\|"); 
-            for(int i=0;i<words.length;i++){
-                System.out.println(words[i]);    
-            }  
-
-            //handle the message
-            fnHandlePublish(words);
-
+            // Split info in an array call the handler function
+            String[] messageFromPublishArray = messageFromPublish.split("\\|");
+            fnHandlePublish(messageFromPublishArray);
         }
 
         @Override
         public void deliveryComplete(IMqttDeliveryToken token) {
             try {
                 // System.out.println("Pub complete" + new String(token.getMessage().getPayload()));
-                if(token.getMessage()==null){
+                if(token.getMessage() == null){
                     messages_completed++;
                     System.out.println("Messages completed:" + messages_completed);
                 }
             }
-            catch(MqttException exception){
+            catch(MqttException exception) {
                 exception.getMessage();
             }
         }
@@ -159,44 +127,73 @@ public class EdgeServer {
 
     static void fnHandlePublish(String[] messageFromPublish){
 
-        // CREATE TIMESTAMP
+        // Information given through mqtt
+        double longitude = Double.parseDouble(messageFromPublish[0]);
+        double latitude = Double.parseDouble(messageFromPublish[1]);
+        double battery = Double.parseDouble(messageFromPublish[2]);
+        double current_smoke = Double.parseDouble(messageFromPublish[3]);
+        double current_gas = Double.parseDouble(messageFromPublish[4]);
+        double current_temperature = Double.parseDouble(messageFromPublish[5]);
+        double current_radiation = Double.parseDouble(messageFromPublish[6]);
+
+        String timestamp = fnGetTimestamp();
+        double distance_from_android = fnCalculateDistanceFromAndroid(longitude, latitude);
+        int danger_level = fnComputeDangerLevel(current_smoke, current_gas, current_temperature, current_radiation);
+
+        // Contruct SQL query
+        String sqlInsert = "INSERT INTO events VALUES(NULL, " + timestamp + ", " + longitude + ", " + latitude + ", " + danger_level + ", " + distance_from_android + ", " + battery + ", " + current_smoke + ", " + current_gas + ", " + current_temperature + ", " + current_radiation + ")";
+
+        // Create connection with DB and execute query
+        Connection conn = fnGetDatabaseConnection();
+        try {
+            PreparedStatement statement = conn.prepareStatement(sqlInsert);
+            int row = statement.executeUpdate();
+            System.out.println("mySQL affected rows" + row);
+        }
+        catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    /********************
+    ** ASSET FUNCTIONS **
+    ********************/
+    // Get a connection with the DB
+    public static Connection fnGetDatabaseConnection(){
+        Connection conn = null;
+        try{
+            String driver = "com.mysql.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/preventiondb?characterEncoding=latin1";
+            String user = "root";
+            String password = "Project123!";
+
+            conn = DriverManager.getConnection(url, user, password);   
+        } 
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+    // Get current date formatted for mysql
+    static String fnGetTimestamp(){
         long millisNow = System.currentTimeMillis();
         SimpleDateFormat timestamp2 = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Date result = new Date(millisNow);
         String timestamp = "'" + timestamp2.format(result) + "'";
+        return timestamp;
+    }
 
-        // Information given through mqtt
-        double longitude = 10.01;
-        double latitude = 9.9;
-        int danger_level = 1;            
-        int battery = 68;
-        int sensor_type = 0;
-        double sensor_measurement = 89.71;
+    // Calculate distance of device from the android
+    static Double fnCalculateDistanceFromAndroid(Double x, Double y){
+        return x * y;
+    }
 
-        // Calculate danger level based on the type of sensor
-        //int danger_level;
-
-        // Limits and thresholds
-        double smoke_min_value = 0;
-        double smoke_max_value = 0.25;
-        double smoke_upper_threshold = 0.14;
-        double gas_min_value = 0;
-        double gas_max_value = 11;
-        double gas_upper_threshold = 1.0065; // 9.15%
-        double temperature_min_value = -5;
-        double temperature_max_value = 80;
-        double temperature_upper_threshold = 50;
-        double radiation_min_value = 0;
-        double radiation_max_value = 11;
-        double radiation_upper_threshold = 6;
-
-        // Current values
-        double current_smoke = 0.15;
-        double current_gas = 1.2;
-        double current_temperature = 55;
-        double current_radiation = 7;
-
-        // Values over threshhold
+    // Compute danger level
+    static int fnComputeDangerLevel(Double current_smoke, Double current_gas, Double current_temperature, Double current_radiation){
+        // Values over threshold
         boolean smoke_over_threshold = false;
         if(current_smoke > smoke_upper_threshold) {
             smoke_over_threshold = true;
@@ -214,37 +211,20 @@ public class EdgeServer {
             radiation_over_threshold = true;
         }
 
-        // Smoke + Gas
-        
-
-        /* Calculate distance from the android device */
-        double distance_from_android = 1234.56;
-        
-        String sqlInsert = "INSERT INTO events VALUES(NULL, ?, longitude, latitude, danger_level, distance_from_android, battery, sensor_type, sensor_measurement)";
-        sqlInsert = "INSERT INTO events VALUES(NULL, "+timestamp+", "+longitude+", "+latitude+", "+danger_level+", "+distance_from_android+", "+battery+", "+sensor_type+", "+sensor_measurement+")";
-
-        Connection conn = getConn();
-        try {
-            PreparedStatement statement = conn.prepareStatement(sqlInsert);
-            //statement.setString(1, timestamp);
-            int row = statement.executeUpdate();
-            System.out.println(row);
-        } catch(SQLException ex){
-            System.out.println(ex.getMessage());
-        } finally {
-            // System.out.println("Finally closing connection to DB");            
-            // conn.close();
+        // Calculate danger level based on the sensor measurements
+        int danger_level = 0;
+        if(smoke_over_threshold && gas_over_threshold){
+            danger_level = 2;
+        } else if(!smoke_over_threshold && !gas_over_threshold && temperature_over_threshold && radiation_over_threshold){
+            danger_level = 1;
+        } else if(gas_over_threshold && !smoke_over_threshold && !temperature_over_threshold && !radiation_over_threshold){
+            danger_level = 2;
+        } else if(smoke_over_threshold && gas_over_threshold && temperature_over_threshold && radiation_over_threshold){
+            danger_level = 2;
         }
 
-        //danger_level = 1;
-        //distance_from_android = 1234.56;
-
-
+        return danger_level;
     }
 
-	// void messageArrived(String topic, MqttMessage message) throws MqttException {
-	// 	System.out.println(String.format("[%s] %s", topic, new String(message.getPayload())));
-	// 	System.out.println("\tMessage published on topic 'Area1'");
-	// }
 }
 
